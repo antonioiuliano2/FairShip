c3a6a8c74c2d165269d25508476b69d5a0068db8
Antonio <aiuliano@lxplus.cern.ch>
2018-05-07 16:17:43 +0200

    Updating outdated link definitions

--- charmdet/BoxLinkDef.h
+++ charmdet/BoxLinkDef.h
@@ -14,6 +14,9 @@
 #pragma link C++ class MufluxSpectrometerPoint+;
 #pragma link C++ class MuonTagger+;
 #pragma link C++ class MuonTaggerPoint+;
+#pragma link C++ class MuonTaggerHit+;
+#pragma link C++ class PIXELHit+;
+#pragma link C++ class SciFiHit+;
 #pragma link C++ class Scintillator+;
 #pragma link C++ class ScintillatorPoint+;
 
