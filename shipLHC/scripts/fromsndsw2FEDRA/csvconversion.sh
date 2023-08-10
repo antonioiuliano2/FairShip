5aee9dc223275aea59ddaba705267da13e95ae59
aiulian <antonio.iuliano@cern.ch>
2023-08-10 12:24:49 +0200

    adding mother folder to path

--- shipLHC/scripts/fromsndsw2FEDRA/csvconversion.sh
+++ shipLHC/scripts/fromsndsw2FEDRA/csvconversion.sh
@@ -4,7 +4,7 @@ brickIDs=(11 12 13 14 21 22 23 24 31 32 33 34 41 42 43 44 51 52 53 54) #ehi, I h
 for ibrick in $(seq 0 19)
  do
   cd b0000${brickIDs[ibrick]}
-  cp $SNDSW_ROOT/scripts/fromsndsw2FEDRA/csvconversion.py ./
+  cp $SNDSW_ROOT/shipLHC/scripts/fromsndsw2FEDRA/csvconversion.py ./
   echo "now converting " b0000${brickIDs[ibrick]}
   python csvconversion.py ${brickIDs[ibrick]} ../$1 ../$2 #first parameter signal simulation, second background simulation
   cd ..
